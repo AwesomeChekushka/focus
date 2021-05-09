f53a9d5250a6900d01836dfcda80d7e582925562
Chekushka <awesome.chekushka@gmail.com>
2021-05-09 18:08:15 +0500

    fix(release): fixed semantic-release plugins
    Fixed semantic-release plugins order and add assets

--- release.config.js
+++ release.config.js
@@ -2,15 +2,15 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/gitlab',
     ['@semantic-release/changelog', { changelogFile: 'CHANGELOG.md' }],
     '@semantic-release/npm',
+    '@semantic-release/gitlab',
     [
       '@semantic-release/git',
       {
         assets: ['package.json', 'CHANGELOG.md'],
-        // eslint-disable-next-line no-undef
-        message: `chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}`,
+        // eslint-disable-next-line no-template-curly-in-string
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
       },
     ],
   ],
